0869a3ace50e8fac2daf150d3898b17a98470ebd
Patrick Swickard <swickape@hotmail.com>
2023-11-04 00:08:24 -0400

    3rq think i finally nailed it, clean up later

--- three_requests.py
+++ three_requests.py
@@ -3,8 +3,8 @@ import re
 import json
 
 #request_url = 'https://www.instagram.com/api/v1/users/web_profile_info/?username=vintage_bmore_graffiti'
-#username = 'vintage_bmore_graffiti'
-username = 'cannibal_corpse_limericks'
+username = 'vintage_bmore_graffiti'
+#username = 'cannibal_corpse_limericks'
 
 def get_app_id(username):
   request_url = 'https://www.instagram.com/' + username + '/'
@@ -54,6 +54,7 @@ response_hash = json.loads(response.text)
 
 data = response_hash['data']
 user = data['user']
+user_id = user['id']
 edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
 page_info = edge_owner_to_timeline_media['page_info']
 edges = edge_owner_to_timeline_media['edges']
@@ -76,9 +77,11 @@ print('-----------------------')
 doc_id = '17991233890457762'
 num = '100'
 
+#user_id = '61247467864'
+
 if end_cursor:
   #request_url = 'https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables=%7B%22id%22%3A%2261247467864%22%2C%22after%22%3A%22QVFDNlR2SFNibHlyR1VjekYxejdHeERVYWcxekhmWHZ5ZWJtY2JBa1dQMWpwYURkb0xFVDlncEZ2U1VZTEZrMXM0bHBBZ3UwQlhZSTZlWV9oVlJXRFN5dQ%3D%3D%22%2C%22first%22%3A12%7D'
-  request_url = 'https://www.instagram.com/graphql/query/?doc_id=' + doc_id + '&variables=%7B%22id%22%3A%2261247467864%22%2C%22after%22%3A%22' + end_cursor + '%22%2C%22first%22%3A' + num + '%7D'
+  request_url = 'https://www.instagram.com/graphql/query/?doc_id=' + doc_id + '&variables=%7B%22id%22%3A%22' + user_id + '%22%2C%22after%22%3A%22' + end_cursor + '%22%2C%22first%22%3A' + num + '%7D'
   header_hash = {}
   #header_hash['x-ig-app-id'] = '936619743392459'
   header_hash['x-ig-app-id'] = app_id
