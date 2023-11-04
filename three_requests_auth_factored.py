de189e467eade8d3503f3f801d9d33664a5ee4dc
patrick.swickard@amplyfigroup.com <patrick.swickard@amplyfigroup.com>
2023-11-04 16:24:17 -0400

    get more results

--- three_requests_auth_factored.py
+++ three_requests_auth_factored.py
@@ -8,7 +8,7 @@ username = 'bugbobbie'
 username = 'dont_fear_the_millimeter'
 
 def get_app_id(username):
-  debug = True
+  debug = False
   request_url = 'https://www.instagram.com/' + username + '/'
 
   headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0'}
@@ -35,8 +35,6 @@ def get_app_id(username):
         app_id = app_id_hits[0]
         return app_id
 
-app_id = get_app_id(username)
-print(app_id)
 def get_first_set(username,app_id):
   request_url = 'https://www.instagram.com/api/v1/users/web_profile_info/?username=' + username
   header_hash = {
@@ -45,19 +43,16 @@ def get_first_set(username,app_id):
   # if/when this breaks try the hard-coded version
   #header_hash['x-ig-app-id'] = '936619743392459'
   sessionid = '61247467864:QjScDRfinWftSQ%3A8%3AAYeBWdjl72x1BlG3IxhmLc2QNJRl4pZznA_p6CV8-Q'
-#  header_hash['Cookie'] = 'sessionid=61247467864:QjScDRfinWftSQ%3A8%3AAYeBWdjl72x1BlG3IxhmLc2QNJRl4pZznA_p6CV8-Q; ds_user_id=CAFE'
   header_hash['Cookie'] = 'sessionid=' + sessionid + '; ds_user_id=CAFE'
   header_hash['x-ig-app-id'] = app_id
   headers = header_hash
   response = requests.get(request_url, headers=headers)
-  #print(response.text)
   response_hash = json.loads(response.text)
   return response_hash
 
 def print_links_from_response_hash(response_hash):
   data = response_hash['data']
   user = data['user']
-  #user_id = user['id']
   edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
   page_info = edge_owner_to_timeline_media['page_info']
   edges = edge_owner_to_timeline_media['edges']
@@ -65,15 +60,10 @@ def print_links_from_response_hash(response_hash):
   end_cursor = ''
   if has_next_page:
     end_cursor = page_info['end_cursor']
-
-  # this drills down and prints links to the full image for the top twelve results for this user
-  # only works for public since not using authentication
   for thisedge in edges:
     node = thisedge['node']
     display_url = node['display_url']
     print(display_url)
-
-print('-----------------------')
 
 def get_user_id_from_response_hash(response_hash):
   data = response_hash['data']
@@ -92,28 +82,12 @@ def get_end_cursor_from_response_hash(response_hash):
     end_cursor = page_info['end_cursor']
   return end_cursor
 
-response_hash = get_first_set(username,app_id)
-
-print('HELLO')
-print(response_hash)
-print('HI')
-
-# hard-coded, hopefully always the same
-doc_id = '17991233890457762'
-user_id = get_user_id_from_response_hash(response_hash)
-end_cursor = get_end_cursor_from_response_hash(response_hash)
-num = '50'
-
 def get_next_response_hash(doc_id,user_id,end_cursor,num):
   if end_cursor:
-    #request_url = 'https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables=%7B%22id%22%3A%2261247467864%22%2C%22after%22%3A%22QVFDNlR2SFNibHlyR1VjekYxejdHeERVYWcxekhmWHZ5ZWJtY2JBa1dQMWpwYURkb0xFVDlncEZ2U1VZTEZrMXM0bHBBZ3UwQlhZSTZlWV9oVlJXRFN5dQ%3D%3D%22%2C%22first%22%3A12%7D'
     request_url = 'https://www.instagram.com/graphql/query/?doc_id=' + doc_id + '&variables=%7B%22id%22%3A%22' + user_id + '%22%2C%22after%22%3A%22' + end_cursor + '%22%2C%22first%22%3A' + num + '%7D'
     header_hash = {
     }
-    #header_hash['x-ig-app-id'] = '936619743392459'
-    # OBOE sessionid pos 1, steal a fresh one
     sessionid = '61247467864:QjScDRfinWftSQ%3A8%3AAYeBWdjl72x1BlG3IxhmLc2QNJRl4pZznA_p6CV8-Q'
-#    header_hash['Cookie'] = 'sessionid=61247467864:QjScDRfinWftSQ%3A8%3AAYeBWdjl72x1BlG3IxhmLc2QNJRl4pZznA_p6CV8-Q; ds_user_id=CAFE'
     header_hash['Cookie'] = 'sessionid=' + sessionid + '; ds_user_id=CAFE'
     header_hash['x-ig-app-id'] = app_id
     headers = header_hash
@@ -121,8 +95,33 @@ def get_next_response_hash(doc_id,user_id,end_cursor,num):
     response_hash = json.loads(response.text)
     return response_hash
 
+app_id = get_app_id(username)
+response_hash = get_first_set(username,app_id)
+print_links_from_response_hash(response_hash)
+
+# hard-coded, hopefully always the same
+doc_id = '17991233890457762'
+user_id = get_user_id_from_response_hash(response_hash)
+end_cursor = get_end_cursor_from_response_hash(response_hash)
+num = '50'
 next_response_hash = get_next_response_hash(doc_id,user_id,end_cursor,num)
-
-print('!!!!!!!!!!!!!!!!!!!!!')
-
 print_links_from_response_hash(next_response_hash)
+
+# another round anyone?
+
+doc_id = '17991233890457762'
+user_id = user_id
+end_cursor = get_end_cursor_from_response_hash(next_response_hash)
+num = '50'
+next_response_hash = get_next_response_hash(doc_id,user_id,end_cursor,num)
+print_links_from_response_hash(next_response_hash)
+
+# another round anyone?
+
+doc_id = '17991233890457762'
+user_id = user_id
+end_cursor = get_end_cursor_from_response_hash(next_response_hash)
+num = '50'
+next_response_hash = get_next_response_hash(doc_id,user_id,end_cursor,num)
+print_links_from_response_hash(next_response_hash)
+
