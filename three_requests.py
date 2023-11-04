62514ab61d4f7a398761d8bb6b7bab5ee8bbba22
Patrick Swickard <swickape@hotmail.com>
2023-11-03 23:31:26 -0400

    3rq checkin

--- three_requests.py
+++ three_requests.py
@@ -3,7 +3,8 @@ import re
 import json
 
 #request_url = 'https://www.instagram.com/api/v1/users/web_profile_info/?username=vintage_bmore_graffiti'
-username = 'vintage_bmore_graffiti'
+#username = 'vintage_bmore_graffiti'
+username = 'cannibal_corpse_limericks'
 
 def get_app_id(username):
   request_url = 'https://www.instagram.com/' + username + '/'
@@ -41,7 +42,7 @@ def get_app_id(username):
 app_id = get_app_id(username)
 print(app_id)
 
-request_url = 'https://www.instagram.com/api/v1/users/web_profile_info/?username=cannibal_corpse_limericks'
+request_url = 'https://www.instagram.com/api/v1/users/web_profile_info/?username=' + username
 header_hash = {}
 #header_hash['x-ig-app-id'] = '936619743392459'
 header_hash['x-ig-app-id'] = app_id
@@ -54,7 +55,52 @@ response_hash = json.loads(response.text)
 data = response_hash['data']
 user = data['user']
 edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
+page_info = edge_owner_to_timeline_media['page_info']
 edges = edge_owner_to_timeline_media['edges']
+has_next_page = page_info['has_next_page']
+end_cursor = ''
+if has_next_page:
+  end_cursor = page_info['end_cursor']
+
+# this drills down and prints links to the full image for the top twelve results for this user
+# only works for public since not using authentication
+for thisedge in edges:
+  node = thisedge['node']
+  display_url = node['display_url']
+  print(display_url)
+
+print('-----------------------')
+
+# hard-coded, hopefully always the same
+# okay maybe not - if I replace cancorp then this gives me next items for vintage graf
+doc_id = '17991233890457762'
+num = '100'
+
+if end_cursor:
+  #request_url = 'https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables=%7B%22id%22%3A%2261247467864%22%2C%22after%22%3A%22QVFDNlR2SFNibHlyR1VjekYxejdHeERVYWcxekhmWHZ5ZWJtY2JBa1dQMWpwYURkb0xFVDlncEZ2U1VZTEZrMXM0bHBBZ3UwQlhZSTZlWV9oVlJXRFN5dQ%3D%3D%22%2C%22first%22%3A12%7D'
+  request_url = 'https://www.instagram.com/graphql/query/?doc_id=' + doc_id + '&variables=%7B%22id%22%3A%2261247467864%22%2C%22after%22%3A%22' + end_cursor + '%22%2C%22first%22%3A' + num + '%7D'
+  header_hash = {}
+  #header_hash['x-ig-app-id'] = '936619743392459'
+  header_hash['x-ig-app-id'] = app_id
+  headers = header_hash
+  response = requests.get(request_url, headers=headers)
+  response_hash = json.loads(response.text)
+  print(response.text)
+
+print('***************')
+print(end_cursor)
+
+
+print('!!!!!!!!!!!!!!!!!!!!!')
+data = response_hash['data']
+user = data['user']
+edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
+page_info = edge_owner_to_timeline_media['page_info']
+edges = edge_owner_to_timeline_media['edges']
+has_next_page = page_info['has_next_page']
+end_cursor = ''
+if has_next_page:
+  end_cursor = page_info['end_cursor']
 
 # this drills down and prints links to the full image for the top twelve results for this user
 # only works for public since not using authentication
