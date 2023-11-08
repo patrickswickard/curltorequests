6dd4735c6afca4f02b6b743897e1b4d1a144e3c7
Patrick Swickard <swickape@hotmail.com>
2023-11-08 10:58:56 -0500

    caps hack

---
+++ three_requests_auth_factored_caps.py
@@ -0,0 +1,171 @@
+import requests
+import re
+import json
+import shutil
+import mysecret
+
+#username = 'vintage_bmore_graffiti'
+#username = 'cannibal_corpse_limericks'
+#username = 'bugbobbie'
+username = 'dont_fear_the_millimeter'
+
+secret = mysecret.Mysecret()
+sessionid = secret.sid
+print(sessionid)
+
+all_photos_list = []
+
+#tempoutfile = open('TEMPJSONOUTFILE.json', 'w')
+#tempoutfile.write(json.dumps(THISTHINGIE))
+
+# method to download a single photo, takes url as source and dl target as filename
+def download_single_photo(source,filename):
+  source = source
+  photo_filename = filename
+  url_response = requests.get(source, stream=True)
+  with open(photo_filename, 'wb') as out_file:
+    shutil.copyfileobj(url_response.raw, out_file)
+
+def get_app_id(username):
+  debug = False
+  request_url = 'https://www.instagram.com/' + username + '/'
+
+  headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0'}
+
+  if not debug:
+    proxies = {}
+  else:
+    proxies = {
+      'http' : 'http://localhost:8888',
+      'https' : 'http://localhost:8888',
+    }
+  response = requests.get(request_url, headers=headers, proxies=proxies, verify=False)
+  raw_html = response.text
+  responselines = response.text.splitlines()
+  for thisline in responselines:
+    hit = re.search(r"APP_ID",thisline)
+    if hit:
+      jsonthisline = re.findall(r"<script[^>]*>\s*(.*?)\s*</script>",thisline)
+      if jsonthisline:
+        jsontext = jsonthisline[0]
+        # this json is so disorganized it's not even worth parsing
+        #thishash = json.loads(jsontext)
+        app_id_hits = re.findall(r"\"APP_ID\":\"(.*?)\"",jsontext)
+        app_id = app_id_hits[0]
+        return app_id
+
+def get_first_set(username,app_id,sessionid):
+  request_url = 'https://www.instagram.com/api/v1/users/web_profile_info/?username=' + username
+  header_hash = {
+  }
+  # this is probably hard-coded but we parse it anyway
+  # if/when this breaks try the hard-coded version
+  #header_hash['x-ig-app-id'] = '936619743392459'
+  header_hash['Cookie'] = 'sessionid=' + sessionid + '; ds_user_id=CAFE'
+  header_hash['x-ig-app-id'] = app_id
+  headers = header_hash
+  response = requests.get(request_url, headers=headers)
+  response_hash = json.loads(response.text)
+  tempoutfile = open('TEMPJSONOUTFILE.json', 'w')
+  tempoutfile.write(response.text)
+  return response_hash
+
+def print_links_from_response_hash(response_hash):
+  data = response_hash['data']
+  user = data['user']
+  edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
+  page_info = edge_owner_to_timeline_media['page_info']
+  edges = edge_owner_to_timeline_media['edges']
+  has_next_page = page_info['has_next_page']
+  end_cursor = ''
+  if has_next_page:
+    end_cursor = page_info['end_cursor']
+  for thisedge in edges:
+    node = thisedge['node']
+    display_url = node['display_url']
+    print(display_url)
+
+def list_links_from_response_hash(response_hash):
+  batch_list = []
+  data = response_hash['data']
+  user = data['user']
+  edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
+  page_info = edge_owner_to_timeline_media['page_info']
+  edges = edge_owner_to_timeline_media['edges']
+  has_next_page = page_info['has_next_page']
+  end_cursor = ''
+  if has_next_page:
+    end_cursor = page_info['end_cursor']
+  for thisedge in edges:
+    node = thisedge['node']
+    display_url = node['display_url']
+    if node.get('accessibility_caption',''):
+      accessibility_caption = node['accessibility_caption']
+      print(accessibility_caption)
+    if node.get('edge_media_to_caption',''):
+      captionlist = node['edge_media_to_caption']['edges']
+      if captionlist:
+        caption = captionlist[0]
+        print(caption['node']['text'])
+    batch_list.append(display_url)
+  return batch_list
+
+def get_user_id_from_response_hash(response_hash):
+  data = response_hash['data']
+  user = data['user']
+  user_id = user['id']
+  return user_id
+
+def get_end_cursor_from_response_hash(response_hash):
+  data = response_hash['data']
+  user = data['user']
+  edge_owner_to_timeline_media = user['edge_owner_to_timeline_media']
+  page_info = edge_owner_to_timeline_media['page_info']
+  has_next_page = page_info['has_next_page']
+  end_cursor = ''
+  if has_next_page:
+    end_cursor = page_info['end_cursor']
+  return end_cursor
+
+def get_next_response_hash(doc_id,user_id,end_cursor,num,sessionid):
+  if end_cursor:
+    request_url = 'https://www.instagram.com/graphql/query/?doc_id=' + doc_id + '&variables=%7B%22id%22%3A%22' + user_id + '%22%2C%22after%22%3A%22' + end_cursor + '%22%2C%22first%22%3A' + num + '%7D'
+    header_hash = {
+    }
+    header_hash['Cookie'] = 'sessionid=' + sessionid + '; ds_user_id=CAFE'
+    header_hash['x-ig-app-id'] = app_id
+    headers = header_hash
+    response = requests.get(request_url, headers=headers)
+    response_hash = json.loads(response.text)
+    tempoutfile = open('TEMPJSONOUTFILENEXT.json', 'w')
+    tempoutfile.write(response.text)
+    return response_hash
+
+app_id = get_app_id(username)
+response_hash = get_first_set(username,app_id,sessionid)
+print_links_from_response_hash(response_hash)
+this_list = list_links_from_response_hash(response_hash)
+all_photos_list = all_photos_list + this_list
+
+# hard-coded, hopefully always the same
+doc_id = '17991233890457762'
+user_id = get_user_id_from_response_hash(response_hash)
+end_cursor = get_end_cursor_from_response_hash(response_hash)
+num = '50'
+
+while end_cursor:
+  next_response_hash = get_next_response_hash(doc_id,user_id,end_cursor,num,sessionid)
+  print_links_from_response_hash(next_response_hash)
+  this_list = list_links_from_response_hash(next_response_hash)
+  all_photos_list = all_photos_list + this_list
+  doc_id = doc_id
+  user_id = user_id
+  end_cursor = get_end_cursor_from_response_hash(next_response_hash)
+  num = '50'
+  print('!!!!!!!!!!!!!!!')
+
+print('GROOGROOGROOGROOGROOGROOGROO')
+print(json.dumps(all_photos_list))
+outfilename = 'all_photos_list4.json'
+thisoutfile = open(outfilename, 'w')
+thisoutfile.write(json.dumps(all_photos_list))
