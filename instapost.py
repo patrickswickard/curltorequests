32c373479f0eeef718d6f493626af46690cc7bca
Patrick Swickard <swickape@hotmail.com>
2023-11-12 13:52:49 -0500

    instapost adjusted

---
+++ instapost.py
@@ -0,0 +1,25 @@
+class Instapost:
+  def __init__(self):
+    self.id = ''
+    self.shortcode = ''
+    self.dimensions = {}
+    self.dimensions['width'] = 0
+    self.dimensions['height'] = 0
+    self.display_url = ''
+    self.tagged_user_list = []
+    self.fact_check_overall_rating = ''
+    self.fact_check_information = ''
+    self.gating_info = ''
+    self.sharing_friction_info = ''
+    self.media_overlay_info = ''
+    self.media_preview = ''
+    self.owner = ''
+    self.is_video = False
+    self.has_upcoming_event = False
+    self.accessibility_caption = ''
+    self.tagged_user_list = []
+    self.caption = ''
+    self.userid = ''
+    self.username = ''
+    self.location = ''
+    self.sidecar_to_children_list = []
